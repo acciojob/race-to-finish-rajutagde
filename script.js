156f1c9877f333eea6e7ae2404f1183408c6d59d
Rajkirantagde <121598976+rajutagde@users.noreply.github.com>
2023-04-22 13:18:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 window.promises = [];
+window.promises = [];
 
+for (let i = 0; i < 5; i++) {
+promises.push(
+new Promise((resolve) => {
+const delay = Math.floor(Math.random() * 5) + 1;
+setTimeout(() => resolve(delay), delay * 1000);
+})
+);
+}
 // Do not change the code above this
 // add your promises to the array `promises`
